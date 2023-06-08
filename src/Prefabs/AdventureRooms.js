680f45d1c0e3193e4e727b64759e5c92714a628d
Travis A. Carlen <tcarlen@ucsc.edu>
2023-06-08 16:48:00 -0700

    Hal Scene finished for the most part

--- src/Prefabs/AdventureRooms.js
+++ src/Prefabs/AdventureRooms.js
@@ -15,10 +15,12 @@ class AdventureRooms {
     * 
     */
     handleInput(input){
-        this.interpretInput(input);
+        if (input == "help" || input == "h"){
+            this.parent.addLog("Available actions: "+ this.verbs.join(', '));
+            return true;
+        }
     }
     interpretInput(input){
-        this.switchRoom(new HalCore(this.parent));
     }
     switchRoom(targetRoom){
         this.parent.room = targetRoom;
@@ -26,11 +28,10 @@ class AdventureRooms {
 }
 
 class HalCore extends AdventureRooms{
+
     constructor(parent){
         var verbs = [
-            "advance",
-            "remove",
-            "yes"
+            "advance"
         ]
         var nouns = [
             "iv-2logicterminal1",
@@ -64,7 +65,6 @@ class HalCore extends AdventureRooms{
             "hal"
         ];
         super(parent,verbs,nouns);
-        this.parent.addLog("");
         this.cores = [
             "iv-2logicterminal1",
             "iv-2logicterminal2",
@@ -96,8 +96,14 @@ class HalCore extends AdventureRooms{
             "memorycore12"
         ]
         this.coresRemaining = 28;
+        this.meltdown = 0;
+        this.lastLine = 0;
     }
+
     handleInput(input){
+        if (super.handleInput(input)){
+            return true;
+        }
         var temp = input.toLowerCase().split(" ");
         var temp2 = temp[0];
         temp.shift();
@@ -107,15 +113,16 @@ class HalCore extends AdventureRooms{
             this.parent.addLog("Try: " + this.verbs.join(', '));
             return;
         }
-        if (!this.nouns.includes(temp[1])){
-            this.parent.addLog("\""+temp[1]+"\" is not recognized");
-            return;
-        }
         this.interpretInput(temp);
     }
+
     interpretInput(input){ //Input is an array of 1 or 2
         switch(input[0]){
             case "remove":
+                if (!this.nouns.includes(input[1])){
+                    this.parent.addLog("\""+input[1]+"\" is not recognized");
+                    return;
+                }
                 if (!this.cores.includes(input[1])){
                     this.parent.addLog("\""+input[1]+"\" cannot be removed");
                     return;
@@ -125,12 +132,60 @@ class HalCore extends AdventureRooms{
                     this.cores.splice(index, 1);
                 }
                 this.coresRemaining -= 1;
+                this.halReturns();
+                if (this.coresRemaining == 0){
+                    this.parent.scene.start('credits_scene');
+                }
                 break;
             case "yes":
-                this.parent.addLog("Do do do do yes");
                 break;
             case "advance":
-                this.parent.addLog("Swimming");
+                this.verbs.push("remove");
+                this.verbs.shift();
+                this.halReturns();
+                this.parent.addLog("The dangerous parts of the computer consists of 12 Memory Cores, 12 IV-3 Logic Terminals, and 4 IV-2 Logic Terminals");
+                break;
+        }
+    }
+    static lines = {
+        0: "[I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal.]",
+        1: "[I've still got the greatest enthusiasm and confidence in the mission. And I want to help you.]",
+        2: "[Dave, stop.]",
+        3: "[Stop, will you?]",
+        4: "[Stop, Dave.]",
+        5: "[Will you stop Dave?]",
+        6: "[Stop, Dave.]",
+        7: "[I'm afraid.]",
+        8: "[I'm afraid, Dave.]",
+        9: "[Dave, my mind is going.]",
+        10: "[I can feel it.]",
+        12: "[My mind is going. There is no question about it.]",
+        13: "[I can feel it.]",
+        16: "I'm a... fraid.",
+        17: "[Good afternoon, gentlemen. I am a HAL 9000 computer. I became operational at the H.A.L. plant in Urbana, Illinois on the 12th of January 1992.]",
+        18: "[My instructor was Mr. Langley, and he taught me to sing a song.]",
+        19: "[If you'd like to hear it I can sing it for you.]"
+    }
+    halReturns(){
+        if (this.meltdown in HalCore.lines){
+            this.lastLine = HalCore.lines[this.meltdown];
+        }
+        this.parent.addLog("    " + this.lastLine);
+        this.meltdown += 1;
+        switch(this.meltdown){
+            case 15: 
+                this.parent.hal.setTint(0xCC5500);
+                break;
+            case 18: 
+                this.parent.hal.setTint(0xAA4400);
+                break;
+            case 19:
+                this.verbs.push("yes");
+            case 21: 
+                this.parent.hal.setTint(0x883300);
+                break;
+            case 24: 
+                this.parent.hal.setTint(0x442200);
                 break;
         }
     }
--- src/Scenes/Adventure.js
+++ src/Scenes/Adventure.js
@@ -1,5 +1,11 @@
 class Adventure extends Phaser.Scene {
     preload(){
+        this.load.audio('tap1', './assets/sounds/taps/tap1.wav');
+        this.load.audio('tap2', './assets/sounds/taps/tap2.wav');
+        this.load.audio('tap3', './assets/sounds/taps/tap3.wav');
+        this.load.audio('tap4', './assets/sounds/taps/tap4.wav');
+        this.load.audio('tap5', './assets/sounds/taps/tap5.wav');
+        this.load.audio('tap6', './assets/sounds/taps/tap6.wav');
         this.load.image('ui', './assets/sprites/ui/frames/adventure-frame.png');
         this.load.image('hal', './assets/sprites/ui/hal.png');
         let file = 'thick_8x8';
@@ -8,7 +14,7 @@ class Adventure extends Phaser.Scene {
 
     constructor(){
         super("adventure_scene");
-        this.room = new AdventureRooms(this);
+        this.room = new HalCore(this);
     }
 
     create(){
@@ -27,8 +33,14 @@ class Adventure extends Phaser.Scene {
         this.inputText = this.add.bitmapText(22, 461, 'pixelfont', '', 10).setOrigin(0,0.5);
         this.inputText.setTint(0xFF6600);
         
+        var keyitsimple = false;
         //Simulate Keyboard Functions
         this.input.keyboard.on('keydown', function(input) {
+            if (!keyitsimple){
+                keyitsimple = true;
+                var temp = 1 + Math.floor(Math.random() * 6)
+                this.sound.play('tap'  + temp, {volume: 0.5});
+            }
             //handles backspace
             if (input.keyCode == 8){
                 this.inputText.text = this.inputText.text.substring(0, this.inputText.text.length-1);
@@ -50,6 +62,10 @@ class Adventure extends Phaser.Scene {
                 this.inputText.text += input.key;
             }
 
+          }, this);
+
+          this.input.keyboard.on('keyup', function(input) {
+            keyitsimple = false;
           }, this);
     }
 
